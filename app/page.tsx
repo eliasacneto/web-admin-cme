80fbeeb623b9f091bc18b354253436d9bc2f81c9
Thiago Morais <118579263+thiagomorais16@users.noreply.github.com>
2024-07-12 23:58:08 -0300

    create formik e yup and update Login

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,36 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Form, Formik, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 import Image from "next/image";
 import Link from "next/link";
+import { log } from "console";
+import { useRouter } from "next/navigation";
 
 export default function Login() {
-  return (
+  
+  const router = useRouter();
+  
+  const validation = Yup.object().shape({
+    email: Yup.string().email('Email inválido'). required('Email é Obrigatório'),
+    password: Yup.string().required('Senha é obrigatória')
+  });
+
+    /*valor Inicial Macho se ligue */
+    const valorInicial = {
+      email: '',
+      password: ''
+    } 
+
+    const submitFormik = (values: { email: string; password: string }) => {
+      console.log(values);
+
+      /* se der bom vai pra página  */
+      router.push("/dashboard");
+    };
+    return (
+
     <>
       <div className="flex flex-col lg:flex-row gap-10">
         <div className="flex w-full h-60 lg:h-screen lg:w-[70%] bg-slate-500 bg-login bg-blend-multiply text-white">
@@ -32,19 +58,41 @@ export default function Login() {
             <span>Entre com o seu e-mail e senha</span>
           </div>
           <div className="flex flex-col gap-4 justify-center w-[70%] items-center">
-            <form className="flex flex-col w-full gap-2  ">
-              <Input type="email" placeholder="email@email.com" />
-              <Input type="password" placeholder="******" />
-              <Link href="/dashboard" className="">
+              <Formik
+                initialValues={valorInicial}
+                validationSchema={validation}
+                onSubmit={submitFormik}
+              >
+                {({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
+
+               <Form noValidate onSubmit={handleSubmit} className="flex flex-col w-full gap-2"> 
+              <Field 
+               type="email" 
+               name="email"
+               placeholder="email@email.com"
+               as={Input}
+              className={`input ${errors.email && touched.email ? 'input-error' : ''}`}
+                />
+                <ErrorMessage name="email" component="div" className="error"/>
+              <Field
+              type="password" 
+              name="password"
+              placeholder="******"
+              as={Input}
+              className={`input ${errors.password && touched.password ? 'input-error': ''}`}
+              />
+                <ErrorMessage name="password" component="div" className="error"/>
                 <Button className="mt-2 bg-[#A7B928] font-bold shadow-lg hover:bg-[#95a140] transform duration-500 w-full">
                   Entrar
                 </Button>
-              </Link>
-            </form>
-            <Button variant={"ghost"}>Esqueci minha senha</Button>
-          </div>
+              </Form>
+              )}
+               </Formik>
+                <Button variant={"ghost"}>Esqueci minha senha</Button>
+            </div>          
           <footer className="flex flex-col items-center  absolute bottom-0 pb-5 text-sm">
             <p>
+
               ©2024{" "}
               <Link
                 href="#"
